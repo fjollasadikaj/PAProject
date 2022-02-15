5b53a5178aa79ff3b4bfaa1f4db26575fc742376
MijadaKrasniqi <96790948+MijadaKrasniqi@users.noreply.github.com>
2022-02-15 19:05:31 +0100

    Ndarja e te dhenave te trajnimit dhe testimit
    Ndarja e te dhenave per trajnim dhe testim sipas modelit sekuencial si dhe vendosja e shtresave per modelimin e te dhenave bazuar ne ndarjen e te dhenave

--- Traffic_sign.py
+++ Traffic_sign.py
@@ -35,3 +35,31 @@ data = np.array(data)
 labels = np.array(labels)
 
 print(data.shape, labels.shape)
+#Ndarja e të dhënave të trajnimit dhe testimi
+X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=42)
+print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+
+
+y_train = to_categorical(y_train, 43)
+y_test = to_categorical(y_test, 43)
+
+#Ndertimi i modelit sekuencial
+model = Sequential()
+model.add(Conv2D(filter=32, kernel_size(5,5), activation='relu', input_shape=X_train.shape[1:]))
+model.add(Conv2D(filter=32, kernel_size(5,5), activation='relu'))
+model.add(MaxPool2D(pool_size=(2,2)))
+model.add(Dropout(rate=0.25))
+model.add(Conv2D(filters=64, kernel_size=(3,3), activation='relu'))
+model.add(Conv2D(filters=64, kernel_size=(3,3), activation='relu'))
+model.add(MaxPool2D(pool_size=(2,2)))
+model.add(Dropout(rate=0.25))
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(rate=0.5))
+model.add(Dense(43, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+epochs = 15
+history = model.fit(X_train, y_train, batch_size=32, epochs=epochs, validation_data=(X_test, y_test))
+model.save("my_model.h5")
+
